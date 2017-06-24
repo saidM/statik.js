a975de55828ff91922e570fae40599e00c87d5d4
Saïd <sayid.mimouni@gmail.com>
2017-06-24 21:51:11 +0200

    Fix error in lib & edit package.json

--- bin/statik.js
+++ bin/statik.js
@@ -6,5 +6,5 @@ const {exec} = require('child_process')
 exec('node /usr/local/lib/node_modules/statik/index.js', (error, stdout, stderr) => {
   if (error) console.error(error)
   if (stdout) console.log(stdout)
-  if (stderr) console.stderr(stderr)
+  if (stderr) console.error(stderr)
 })
