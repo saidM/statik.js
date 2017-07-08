4a698c90b6cccafcc5ebe59c1ce491ebaf1539fc
Saïd <sayid.mimouni@gmail.com>
2017-07-08 11:35:01 +0200

    Rename test files

--- test/read_files.test.js
+++ test/read_files.test.js
@@ -6,12 +6,12 @@ const chai = require('chai'),
 
 describe('readFiles()', () => {
   it('returns the content of each filename passed to the function', (done => {
-    const filenames = ['./test/read_files_test.js']
+    const filenames = ['./test/read_files.test.js']
 
     utils.readFiles(filenames, (err, files) => {
       expect(err).to.be.null
       expect(files.length).to.equal(1)
-      expect(files[0].filename).to.equal('test/read_files_test.js')
+      expect(files[0].filename).to.equal('test/read_files.test.js')
       expect(files[0].content.split('\n')[0]).to.equal("'use strict'") // this is the current file
       done()
     })
